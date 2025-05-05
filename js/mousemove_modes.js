2f71301f95094f3a3208b4eaa75a00ebf8e246c7
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2025-05-05 16:13:46 +0900

    Extract mouse drag handle codes into mousemove_modes.js

---
+++ js/mousemove_modes.js
@@ -0,0 +1,71 @@
+class MouseHandler {
+    handleMouseDown(e) { throw new Error("Must override handleMouseDown"); }
+    handleMouseMove(e) { throw new Error("Must override handleMouseMove"); }
+    handleMouseUp(e)   { throw new Error("Must override handleMouseUp"); }
+}
+
+class ModeLineOfFire extends MouseHandler {
+    svg;
+
+    hexName0;
+    hexName1;
+    svgHex0;
+
+    constructor(svg) {
+        super();
+
+        this.svg = svg;
+    }
+
+    handleMouseDown(e) {
+        this.hexName1 = null;
+
+        const [x, y] = transformViewPortCoordToSVG(e.clientX, e.clientY, this.svg);
+
+        this.hexName0 = findHexFromCoord(x, y);
+        if (this.hexName0) {
+            const [cx, cy] = coord(this.hexName0);
+            const elemHex = new SvgHex(cx, cy, rowHeight, {stroke: 'cyan', 'stroke-width': 3});
+            this.svgHex0 = elemHex.toDOM();
+            this.svg.appendChild(this.svgHex0);
+        }
+    }
+
+    handleMouseMove(e) {
+        const [x, y] = transformViewPortCoordToSVG(e.clientX, e.clientY, this.svg);
+        this.hexName1 = findHexFromCoord(x, y);
+
+        if (this.hexName1 && this.hexName1 != this.hexName0) {
+            let lastChild;
+            while ((lastChild = this.svg.lastChild) !== this.svgHex0) {
+                this.svg.removeChild(lastChild);
+            }
+
+            const [cx, cy] = coord(this.hexName1);
+            const elemHex = new SvgHex(cx, cy, rowHeight, {stroke: 'red', 'stroke-width': 3});
+            this.svg.appendChild(elemHex.toDOM());
+        }
+    }
+
+    handleMouseUp(e) {
+        let lastChild;
+        while ((lastChild = this.svg.lastChild) !== this.svgHex0) {
+            this.svg.removeChild(lastChild);
+        }
+        this.svg.removeChild(lastChild);
+
+        if (this.hexName1) {
+            const [x1, y1] = coord(this.hexName0);
+            const [x2, y2] = coord(this.hexName1);
+            const elemLine = new SvgElement('line', {x1, y1, x2, y2, stroke: 'red', class: 'linfOfFire'});
+            this.svg.appendChild(elemLine.toDOM());
+
+            this.hexName1 = null;
+        } else {
+            const elemLinfOfFire = document.querySelector('.linfOfFire');
+            if (elemLinfOfFire) {
+                elemLinfOfFire.remove();
+            }
+        }
+    }
+}
